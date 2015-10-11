e629e9a46f2182a1b05015c53e31fbe052ec8b75
btiwaree <b.tiwaree@jacobs-university.de>
2015-10-11 21:35:47 +0200

    restful get request

--- controller/controllers.js
+++ controller/controllers.js
@@ -9,6 +9,22 @@
     $scope.valueFromService = UtilSrvc.helloWorld("User");
   }]);
 
+  App.controller('GreetingController', ['$scope', '$http', function($scope, $http) {
+    $http.get('http://www.w3schools.com/angular/customers.php').
+      success(function(data) {
+        $scope.names = data.records;
+      })
+
+    $scope.greeting = 'Hola!';
+  }]);
+
+  // App.controller("Hello", ["$scope", function($scope,$http){
+  //   $http.get('http://rest-service.guides.spring.io/greeting').
+  //       success(function(data) {
+  //           $scope.greeting = data;
+  //       });
+  // }]);
+
   App.controller("MyCtrl2", ["$scope", function($scope){
       // if you have many controllers, it's better to separate them into files
   }]);
